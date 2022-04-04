1dbd98208b45cce773e81c0e638968d5cebb3601
Caitlin Boldt <caitlin.boldt@freeman.com>
2022-04-04 16:16:44 -0700

    Redux Toolkit Persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "reduxjs-toolkit-persist";
+import storage from "reduxjs-toolkit-persist/lib/storage";
+import autoMergeLevel1 from "reduxjs-toolkit-persist/lib/stateReconciler/autoMergeLevel1";
 import userReducer from "slices/user/userSlice";
 
-export default configureStore({
-  reducer: {
-    userInfo: userReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  stateReconciler: autoMergeLevel1,
+};
+
+const reducers = combineReducers({
+  userInfo: userReducer,
 });
+
+const _persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: _persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
